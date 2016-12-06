2c44cb7df47f70c22fe2fce4a17cc67999622b08
alanna-wing <alanna.wing@gmail.com>
2016-12-06 15:17:30 +0000

    added multiply function

--- functions_lab/start_point/ruby_functions_practice.rb
+++ functions_lab/start_point/ruby_functions_practice.rb
@@ -1,14 +1,25 @@
+#Add Function
 def add(first_number, second_number)
   return first_number + second_number
 end
 
 add_result = add(1, 2)
-puts "my add result is: #{add_result}"
+puts "Addition: #{add_result}"
 
-
+#Subtract Function
 def subtract(first_number, second_number)
   return first_number - second_number
 end
 
 subtract_result = subtract(10, 5)
-puts "my subtract result is: #{subtract_result}"
+puts "Subtraction: #{subtract_result}"
+
+#Multiply Function
+def multiply(first_number, second_number)
+  return first_number / second_number
+end
+
+multiply_result = multiply(4, 2)
+puts "Multiplication: #{multiply_result}"
+
+#Divide Function
